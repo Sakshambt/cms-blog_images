e2417b7fc5fb84519e1397fdf4f552eb9e137314
Saksham Garg <sakshamgarg524@gmail.com>
2022-07-14 17:35:29 +0530

    featurd post rearranged

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => (
-  <div className="relative h-72 ">
+  <div className="relative h-72 hover:scale-110 transition-all duration-500 ease-in-out transform">
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImages[0].url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
-      <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.date).format('MMM DD, YYYY')}</p>
+      <p className="text-white mb-4 text-shadow font-semibold text-xs ">{moment(post.date).format('MMM DD, YYYY')}</p>
       <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
       {/* <div className="flex items-center absolute bottom-5 w-full justify-center">
         <Image
@@ -24,6 +24,7 @@ const FeaturedPostCard = ({ post }) => (
     </div>
     <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
   </div>
+
 );
 
 export default FeaturedPostCard;
